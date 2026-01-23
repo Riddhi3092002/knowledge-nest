3a941bd9ac57bdade96491cb96e1ee241debade0
riddhi <sharmariddhi309@gmail.com>
2026-01-22 20:21:58 -0500

    frontend updates

--- frontend_app/src/app/app.component.ts
+++ frontend_app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,27 @@ import { Router } from '@angular/router';
 export class AppComponent {
   firstName = '';
   lastName = '';
+  backendReady = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private http: HttpClient) {
     this.loadUser();
+  }
+
+  ngOnInit(): void {
+    this.pingBackend();
+  }
+
+  pingBackend() {
+    this.http.get(environment.backendUrl).subscribe({
+      next: () => {
+        console.log('Backend is awake');
+        this.backendReady = true;
+      },
+      error: () => {
+        console.log('Backend not reachable yet, retrying in 5s...');
+        setTimeout(() => this.pingBackend(), 5000); 
+      }
+    });
   }
 
   isLoggedIn(): boolean {
