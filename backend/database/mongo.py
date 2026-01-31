9a474e571b09ab1f57e264282ba5f013962404d0
riddhi <sharmariddhi309@gmail.com>
2026-01-31 14:29:05 -0500

    mongo updates

--- backend/database/mongo.py
+++ backend/database/mongo.py
@@ -2,6 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
 import certifi
+import ssl
 
 load_dotenv()
 
@@ -10,7 +11,9 @@ MONGO_URI = os.getenv("MONGO_URI")
 client = AsyncIOMotorClient(
     MONGO_URI,
     tls=True,
-    tlsCAFile=certifi.where()
+    tlsCAFile=certifi.where(),
+    tlsAllowInvalidCertificates=False,
+    ssl_cert_reqs=ssl.CERT_REQUIRED
 )
 db = client["SmartStudy"]
 courses_collection = db["Courses"]
