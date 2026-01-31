ca3d3813c4163c89033b600bca5c668b07c19e36
riddhi <sharmariddhi309@gmail.com>
2026-01-31 17:21:54 -0500

    update

--- backend/database/mongo.py
+++ backend/database/mongo.py
@@ -1,17 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
-import certifi
-import ssl
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
+client = AsyncIOMotorClient(MONGO_URI)
 
-client = AsyncIOMotorClient(
-    MONGO_URI,
-    tlsCAFile=certifi.where()
-)
 db = client["SmartStudy"]
 courses_collection = db["Courses"]
 user_collection = db["users"]
