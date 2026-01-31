1269e344de4562ebebcb8f232529062c4806e971
riddhi <sharmariddhi309@gmail.com>
2026-01-31 14:15:12 -0500

    mongodb

--- backend/database/mongo.py
+++ backend/database/mongo.py
@@ -1,11 +1,17 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
+import certifi
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-client = AsyncIOMotorClient(MONGO_URI)
+
+client = AsyncIOMotorClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
 db = client["SmartStudy"]
 courses_collection = db["Courses"]
 user_collection = db["users"]
